aa57a5265d48c28304ff0968cff8d423bdf71c27
vietdoo <dodoviet@icloud.com>
2024-11-27 16:33:17 +0700

    CHORE: Refactor error handling in BaseController; replace ApiResponse with ErrorSerializer for JSON parsing and parameter missing errors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,20 +1,17 @@
+
+
 module Api
   module V1
     class BaseController < ApplicationController
       skip_before_action :verify_authenticity_token
 
       rescue_from JSON::ParserError do
-        render json: ApiResponse.new(status: :bad_request, message: "Invalid JSON").to_h, status: :bad_request
+        render json: ErrorSerializer.new("Invalid JSON").as_json, status: :bad_request
       end
       
       rescue_from ActionController::ParameterMissing do |exception|
-        render json: ApiResponse.new(
-          status: :unprocessable_entity,
-          message: "Missing parameter: #{exception.param}. Please provide a valid #{exception.param}."
-        ).to_h, status: :unprocessable_entity
+        render json: ErrorSerializer.new("Missing parameter: #{exception.param}. Please provide a valid #{exception.param}.").as_json, status: :unprocessable_entity
       end
-
-      
 
     end
   end
